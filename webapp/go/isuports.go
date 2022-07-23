ac8489e3f3ffda1ec2ed77ff48affa0677fe9e37
isucon <isucon@example.com>
2022-07-23 07:34:34 +0000

    :sparkles: bench #13 score=15140, remove flock
    07:32:28.166363 初期化リクエストに成功しました 実装言語:go
    07:32:28.166382 整合性チェックを開始します
    07:32:34.868664 整合性チェックを終了します
    07:32:34.868675 整合性チェックに成功しました
    07:32:34.868701 負荷走行を開始します
    07:32:39.868841 テナント「魁！チェンソー屋さん」を作成しました (類似のログ計1件)
    07:32:39.868841 大会「ペールエール神社 vol.4」を作成しました。参加者が増えます。 (類似のログ計4件)
    07:32:39.868853 参加者が13人増えました
    07:32:44.868889 大会「大きないよかんビーチ大会」を作成しました。参加者が増えます。 (類似のログ計6件)
    07:32:44.868891 参加者が19人増えました
    07:32:49.869752 大会「グレープフルーツのおうち #6」を作成しました (類似のログ計4件)
    07:32:49.869770 参加者が13人増えました
    07:32:54.869022 テナント「ブリリアントミルクはバランスいい」を作成しました (類似のログ計1件)
    07:32:54.869036 参加者が12人増えました
    07:32:54.869030 大会「第6回 中華風梨BETTER」を作成しました (類似のログ計3件)
    07:32:59.869162 テナント「即オンコールのプロ」を作成しました (類似のログ計1件)
    07:32:59.869183 大会「第3回 みかんします」を作成しました。参加者が増えます。 (類似のログ計6件)
    07:32:59.869188 参加者が19人増えました
    07:33:04.869160 大会「本番ぶどう支部 vol.8」を作成しました。参加者が増えます。 (類似のログ計6件)
    07:33:04.869171 参加者が25人増えました
    07:33:09.869654 大会「第4回 川崎軒」を作成しました。参加者が増えます。 (類似のログ計5件)
    07:33:09.869673 参加者が19人増えました
    07:33:14.868873 テナント「新流山おおたかの森企画」を作成しました (類似のログ計1件)
    07:33:14.868877 大会「ブランデー茶杯」を作成しました。参加者が増えます。 (類似のログ計5件)
    07:33:14.868882 参加者が24人増えました
    07:33:19.869738 leaderboardの表示に1秒以上かかったため1人の参加者が離脱しました。
    07:33:19.869745 大会「パワーだらけ杯」を作成しました。参加者が増えます。 (類似のログ計8件)
    07:33:19.869749 参加者が31人増えました
    07:33:24.869241 テナント「エンベデッドベルギービールで再起動」を作成しました (類似のログ計1件)
    07:33:24.869249 大会「流山おおたかの森田中 #6」を作成しました。参加者が増えます。 (類似のログ計4件)
    07:33:24.869254 参加者が13人増えました
    07:33:29.869548 大会「第6回 中野方式」を作成しました。参加者が増えます。 (類似のログ計7件)
    07:33:29.869554 参加者が31人増えました
    07:33:34.869725 テナント「かぼちゃネイティブ」を作成しました (類似のログ計1件)
    07:33:34.869740 大会「のびるライブ.devカップ」を作成しました。参加者が増えます。 (類似のログ計7件)
    07:33:34.869758 参加者が31人増えました
    07:33:34.869791 負荷走行を終了します
    07:33:35.870869 Error 0 (Critical:0)
    07:33:35.870874 PASSED: true
    07:33:35.870877 SCORE: 15140 (+15140 0(0%))

--- webapp/go/isuports.go
+++ webapp/go/isuports.go
@@ -651,11 +651,13 @@ func billingReportByCompetition(ctx context.Context, tenantDB dbOrTx, tenantID i
 	}
 
 	// player_scoreを読んでいるときに更新が走ると不整合が起こるのでロックを取得する
-	fl, err := flockByTenantID(tenantID)
-	if err != nil {
-		return nil, fmt.Errorf("error flockByTenantID: %w", err)
-	}
-	defer fl.Close()
+	/*
+		fl, err := flockByTenantID(tenantID)
+		if err != nil {
+			return nil, fmt.Errorf("error flockByTenantID: %w", err)
+		}
+		defer fl.Close()
+	*/
 
 	// スコアを登録した参加者のIDを取得する
 	scoredPlayerIDs := []string{}
@@ -1114,6 +1116,8 @@ func competitionScoreHandler(c echo.Context) error {
 	}
 	defer f.Close()
 
+	fmt.Printf("The file is %d bytes long", fh.Size)
+
 	r := csv.NewReader(f)
 	headers, err := r.Read()
 	if err != nil {
@@ -1124,11 +1128,13 @@ func competitionScoreHandler(c echo.Context) error {
 	}
 
 	// / DELETEしたタイミングで参照が来ると空っぽのランキングになるのでロックする
-	fl, err := flockByTenantID(v.tenantID)
-	if err != nil {
-		return fmt.Errorf("error flockByTenantID: %w", err)
-	}
-	defer fl.Close()
+	/*
+		fl, err := flockByTenantID(v.tenantID)
+		if err != nil {
+			return fmt.Errorf("error flockByTenantID: %w", err)
+		}
+		defer fl.Close()
+	*/
 	var rowNum int64
 	playerScoreRows := []PlayerScoreRow{}
 	for {
@@ -1310,11 +1316,13 @@ func playerHandler(c echo.Context) error {
 	}
 
 	// player_scoreを読んでいるときに更新が走ると不整合が起こるのでロックを取得する
-	fl, err := flockByTenantID(v.tenantID)
-	if err != nil {
-		return fmt.Errorf("error flockByTenantID: %w", err)
-	}
-	defer fl.Close()
+	/*
+		fl, err := flockByTenantID(v.tenantID)
+		if err != nil {
+			return fmt.Errorf("error flockByTenantID: %w", err)
+		}
+		defer fl.Close()
+	*/
 	pss := make([]PlayerScoreRow, 0, len(cs))
 	for _, c := range cs {
 		ps := PlayerScoreRow{}
@@ -1437,11 +1445,13 @@ func competitionRankingHandler(c echo.Context) error {
 	}
 
 	// player_scoreを読んでいるときに更新が走ると不整合が起こるのでロックを取得する
-	fl, err := flockByTenantID(v.tenantID)
-	if err != nil {
-		return fmt.Errorf("error flockByTenantID: %w", err)
-	}
-	defer fl.Close()
+	/*
+		fl, err := flockByTenantID(v.tenantID)
+		if err != nil {
+			return fmt.Errorf("error flockByTenantID: %w", err)
+		}
+		defer fl.Close()
+	*/
 	pss := []PlayerScoreRow{}
 	if err := tenantDB.SelectContext(
 		ctx,
